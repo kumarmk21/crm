3c48ab491c2532c8eb267ca8350269622f9fbb2c
j.buchanan <jack.buchanan@salesagility.com>
2025-04-11 16:00:18 +0100

    Fix #10377 Update search results to handle multi enum values as currency as defined on the currency type field

--- lib/Search/SearchResults.php
+++ lib/Search/SearchResults.php
@@ -168,7 +168,7 @@ class SearchResults
                 switch ($fieldDef['type']){
                     case 'enum':
                     case 'dynamicenum':
-                    case 'multienum':
+
                         if (isset($obj->field_name_map[$fieldDef['name']]['options']) &&
                             isset($app_list_strings[$obj->field_name_map[$fieldDef['name']]['options']]) &&
                             isset($app_list_strings[$obj->field_name_map[$fieldDef['name']]['options']][$value])
@@ -177,9 +177,31 @@ class SearchResults
                         }
                         break;
 
+                    case 'multienum':
+                        if (isset($obj->field_name_map[$fieldDef['name']]['options']) &&
+                            isset($app_list_strings[$obj->field_name_map[$fieldDef['name']]['options']]) &&
+                            isset($value)
+                        ) {
+                            $multienumValues = unencodeMultienum($value);
+                            $multienumString = '';
+                            $arrayCount = count($multienumValues);
+                            $i = 0;
+                            foreach ($multienumValues as $multienumValue) {
+                                $i++;
+                                $multienumString .= $app_list_strings[$obj->field_name_map[$fieldDef['name']]['options']][$multienumValue];
+                                if($i !== ($arrayCount)) $multienumString .= ", ";
+                            }
+                            $obj->{$fieldDef['name']} = $multienumString;
+                        }
+                        break;
+
                     case 'currency':
                         require_once('modules/Currencies/Currency.php');
-                        $obj->{$fieldDef['name']} = currency_format_number($value);
+                        if(isset($obj->currency_symbol) && isset($obj->currency_id)){
+                            $obj->{$fieldDef['name']} = currency_format_number($value, ["currency_symbol" => $obj->currency_symbol, "currency_id" => $obj->currency_id]);
+                        }else {
+                            $obj->{$fieldDef['name']} = currency_format_number($value);
+                        }
                         break;
                 }
             }
